f679770f5e5cb5e0d62f0832ce25b629683adcae
Mateus Alves <gamertalverxd@gmail.com>
2024-12-26 17:03:32 -0300

    replace pom and persistence corrected

--- src/main/java/application/Program.java
+++ src/main/java/application/Program.java
@@ -1,18 +1,27 @@
 package application;
 
 import dominio.Pessoa;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
 
 public class Program {
 	
 	public static void main (String[] args) {
 		
-		Pessoa p1 = new Pessoa(1,"Lucas de Jesus", "lucasjesus12@gmail.com");
-		Pessoa p2 = new Pessoa(2,"Tiago Oliveira", "oliveiratiago@gmail.com");
-		Pessoa p3 = new Pessoa(3,"Maria Madalena", "mariazinhamada@gmail.com");
-		System.out.println(p1);
-		System.out.println(p2);
-		System.out.println(p3);
+		Pessoa p1 = new Pessoa(null,"Lucas de Jesus", "lucasjesus12@gmail.com");
+		Pessoa p2 = new Pessoa(null,"Tiago Oliveira", "oliveiratiago@gmail.com");
+		Pessoa p3 = new Pessoa(null,"Maria Madalena", "mariazinhamada@gmail.com");
 		
+		EntityManagerFactory emf = Persistence.createEntityManagerFactory("exemplo-jpa");
+		EntityManager em = emf.createEntityManager();
+		
+		em.getTransaction().begin();
+		em.persist(p1);
+		em.persist(p2);
+		em.persist(p3);
+		em.getTransaction().commit();
+		System.out.println("Pronto!");
 	}
 
 }
